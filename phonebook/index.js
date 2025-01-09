160561bc086947287c7faa1e2f32d8fa8bf144ca
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-01-09 12:28:11 -0300

    exercise 3.15

--- phonebook/index.js
+++ phonebook/index.js
@@ -16,33 +16,10 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 const Person = require('./models/contact');
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-
 // get info
 app.get('/info', (request, response) => { 
   const date = new Date()
+  const persons = Person.find({}).then(persons => { return persons })
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
@@ -55,16 +32,33 @@ app.get('/api/persons', (request, response) => {
 
 // get one person
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      }
+      else {
+        console.log('no existe')
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log('error en busqueda')
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 // delete one person
 app.delete('/api/persons/:id', (request, response) => {
-  Person.deleteOne({ _id: request.params.id }).then(deletedPerson => {
-    response.json(deletedPerson)
-  })
+  Person.findByIdAndDelete(request.params.id)
+    .then(deletedPerson => {
+      response.json(deletedPerson)
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 // post one person

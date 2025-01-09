ab6f03352c773870590dc038fe3f3091e038aa7a
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-01-09 19:33:01 -0300

    better validations

--- notes/index.js
+++ notes/index.js
@@ -52,10 +52,6 @@ app.get('/api/notes/:id', (request, response, next) => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -77,14 +73,13 @@ app.delete('/api/notes/:id', (request, response, next) => {
 
 // update one
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -101,8 +96,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 app.use(errorHandler)
